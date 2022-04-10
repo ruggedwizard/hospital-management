5f73e40bbada6a75f8b3bcd6475b8516d9207997
david isaac <davidisaacsurvive@gmail.com>
2022-04-10 02:35:51 +0100

    footer, header and main content completed

--- base/models.py
+++ base/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Department(models.Model):
     Department_name = models.CharField(max_length=250, unique=True, blank=True, null=True)
+    Department_details = models.TextField(max_length=1000, null=True, blank=True)
 
     def __str__(self):
         return self.Department_name + "Department"
