796b47458822d460b4de01fe435af75cd7a30ab5
david isaac <davidisaacsurvive@gmail.com>
2022-04-10 01:10:42 +0100

    login page styled

--- base/urls.py
+++ base/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from base import views
 urlpatterns = [
-    path('login-page/',views.login_page, name="login-page"),
+    path('login-page/',views.login_page, name="login_page"),
+    path('',views.landing_page,name="home_page"),
 ]
--- base/views.py
+++ base/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+def landing_page(request):
+    return render(request,'base/index.html')
+
 def login_page(request):
+
     return render(request,'base/Pages/login_page.html')
+
+def signup_page(request):
+
+    return render(request,'base/Pages/signup_page.html')
