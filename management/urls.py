fc1e009a40e238a729c6e4c0c3b93968088dce29
david isaac <davidisaacsurvive@gmail.com>
2022-04-12 16:48:30 +0100

    management app sider component updated to include updated links

--- management/urls.py
+++ management/urls.py
@@ -4,5 +4,12 @@ urlpatterns = [
     path('', views.welcome_page,name="login_page"),
     path('doctors/',views.doctors_page, name="doctors_page"),
     path('patients/',views.patient_page, name="patients_page"),
+    path('financies/',views.financial_activities, name="financies_page"),
+    path('medicines/',views.medicines, name="medicine_page"),
+    path('donors/',views.donors_page, name="donors_page"),
+    path('beds/',views.beds_page, name="beds_page"),
+    path('reports/',views.reports_page, name="reports_page"),
+    path('profile_page/',views.profile_page, name="profile_page"),
+
 
 ]
--- management/views.py
+++ management/views.py
@@ -10,3 +10,21 @@ def doctors_page(request):
 
 def patient_page(request):
     return render(request,'management/Pages/patient_page.html')
+
+def financial_activities(request):
+    return render(request,'management/Pages/financial_activities_page.html')
+
+def medicines(request):
+    return render(request,'management/Pages/medicine_page.html')
+
+def donors_page(request):
+    return render(request,'management/Pages/donor_page.html')
+
+def beds_page(request):
+    return render(request,'management/Pages/beds_page.html')
+
+def reports_page(request):
+    return render(request,'management/Pages/reports_page.html')
+
+def profile_page(request):
+    return render(request,'management/Pages/profile_page.html')
