67241a14209440bbe2c3899a05596057f8e11c33
david isaac <davidisaacsurvive@gmail.com>
2022-04-12 03:50:15 +0100

    management app updated to include login page and icons for necessary areas

--- management/urls.py
+++ management/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from management import views
 urlpatterns = [ 
-    path('', views.welcome_page,name="management_welcome_page"),
+    path('', views.welcome_page,name="login_page"),
+    path('doctors/',views.doctors_page, name="doctors_page")
 ]
--- management/views.py
+++ management/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def welcome_page(request):
-    return render(request,'management/Pages/dashboard_page.html')
+    return render(request,'management/Pages/login_page.html')
+
+def doctors_page(request):
+    return render(request,'management/Pages/doctors_page.html')
