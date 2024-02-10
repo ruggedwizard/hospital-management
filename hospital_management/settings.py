bf397c76031a4598e1d332708ad38e1a5dc0a234
David Isaac Survive <40686166+ruggedwizard@users.noreply.github.com>
2024-02-10 10:15:58 +0100

    error fixed on secret

--- hospital_management/settings.py
+++ hospital_management/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config['APP_SECRET_KEY']
+SECRET_KEY = 'django-insecure-l44#3(nm2m+byhvl@%aert3xwakx*a^ian#jf$n^%jzzpee67#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
